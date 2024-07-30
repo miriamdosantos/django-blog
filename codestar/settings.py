86f3e4ae9df373ad48f90d94bdb7175698615937
Miriam McCay <mii.santos@yahoo.com.br>
2024-07-30 15:18:54 +0100

    Config space to deploy project at Heroku using gunicorn

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1(w!3g$^63%92qme7j!6tw!c=3exiu&!)l9no^*@v5ftt%f)&r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' ]
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com' ]
 
 
 # Application definition
