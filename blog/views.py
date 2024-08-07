3c1ba93220e5be1062382e1284ff8d7da720f1f0
Miriam McCay <mii.santos@yahoo.com.br>
2024-08-07 08:48:50 +0100

    Fix post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
-from django.shortcuts import get_object_or_404 
+
 
 # Create your views here
 # Generic Class example (PostList):
@@ -24,12 +24,7 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
+    
