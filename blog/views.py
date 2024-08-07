4ba2cc70d8c17c63ae54f9159b87a027658678e9
Miriam McCay <mii.santos@yahoo.com.br>
2024-08-07 08:34:23 +0100

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.shortcuts import get_object_or_404 
 
 # Create your views here
+# Generic Class example (PostList):
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+# Function-based view example (post_detail):
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
