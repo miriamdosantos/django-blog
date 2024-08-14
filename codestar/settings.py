047f9232684bd5a39828969721a0d2ecd222f70b
Miriam McCay <mii.santos@yahoo.com.br>
2024-08-14 11:38:16 +0100

    Add views test e forms test

--- codestar/settings.py
+++ codestar/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import sys
 from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -77,6 +78,9 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com"
